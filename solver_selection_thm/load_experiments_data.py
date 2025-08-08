b210ef49e98c50fd187026a54aa1d145552f96bd
Yury Zabegaev <yuri.zabegaev@uib.no>
2025-08-08 11:06:14 +0000

    checkpoint

--- solver_selection_thm/load_experiments_data.py
+++ solver_selection_thm/load_experiments_data.py
@@ -14,9 +14,14 @@ from itertools import count
 from copy import copy
 from plot_utils import load_data
 from solver_selection_thm.thm_physics import (
-    simulation_name,
+    simulation_name as simulation_name_thm,
     inlet_placements,
     outlet_placements,
+)
+from solver_selection_thm.spe_physics import (
+    simulation_name as simulation_name_spe,
+    X_SLICES,
+    Z_SLICES
 )
 
 
@@ -65,7 +70,70 @@ def load_experiments_data_thm(runs: list[int], random_selection: bool):
             for outlet_placement in outlet_placements_[permutations_outlet]:
                 params["inlet_placement"] = inlet_placement
                 params["outlet_placement"] = outlet_placement
-                sim_name = f"run_{run_idx}_{simulation_name(params)}"
+                sim_name = f"run_{run_idx}_{simulation_name_thm(params)}"
+                if random_selection:
+                    sim_name = f"RANDOM_{sim_name}"
+                try:
+                    data = load_data(f"../stats/{sim_name}.json")
+                    data_row.append(data)
+                    solver_selector.history.load(
+                        f"../stats/solver_selection_history_{sim_name}.npy"
+                    )
+                    solver_selection_history.append(copy(solver_selector.history))
+                except FileNotFoundError:
+                    print("failed to load", sim_name)
+
+    return data_simulations_common, solver_selection_history_common, solver_selector
+
+
+def load_experiments_data_spe(runs: list[int], random_selection: bool):
+
+    data_simulations_common = []
+    solver_selection_history_common = []
+
+    for run_idx in runs:
+
+        with open(f"../stats/spe_solver_space_scheme_run_{run_idx}.pkl", "rb") as f:
+            solver_space_scheme = pickle.load(f)
+
+        # Load permutations
+        with open(f"../stats/spe_permutations_{run_idx}.pkl", "rb") as f:
+            permutations = pickle.load(f)
+            permutations_x = permutations['x']
+            permutations_z = permutations['z']
+
+        solver_space = SolverSpace(
+            solver_space_scheme=solver_space_scheme,
+            solver_scheme_builders=KNOWN_SOLVER_COMPONENTS_THM,
+        )
+        num_solvers = len(solver_space.all_decisions_encoding)
+        print("Num solvers:", num_solvers)
+
+        performance_predictor = PerformancePredictorPassiveAgressive(
+            num_solvers=num_solvers,
+        )
+        solver_selector = SolverSelector(
+            solver_space=solver_space,
+            performance_predictor=performance_predictor,
+        )
+
+        data_simulations = []
+        solver_selection_history = []
+        data_simulations_common.append(data_simulations)
+        solver_selection_history_common.append(solver_selection_history)
+
+
+        Z_SLICES_ = np.array(Z_SLICES)
+        X_SLICES_ = np.array(X_SLICES)
+
+
+        for z_slice in Z_SLICES_[permutations_z]:
+            data_row = []
+            data_simulations.append(data_row)
+            for x_slice in X_SLICES_[permutations_x]:
+                params["x_slice"] = x_slice
+                params["z_slice"] = z_slice
+                sim_name = f"run_{run_idx}_{simulation_name_spe(params)}"
                 if random_selection:
                     sim_name = f"RANDOM_{sim_name}"
                 try:
